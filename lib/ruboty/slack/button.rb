0ee7e20a99c57de3e3bd627785cdc568175bec6b
hkdnet <hkdnet@users.noreply.github.com>
2017-02-08 05:09:07 +0900

    init

---
+++ lib/ruboty/slack/button.rb
@@ -0,0 +1,9 @@
+require "ruboty/slack/button/version"
+
+module Ruboty
+  module Slack
+    module Button
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruboty/slack/button/version.rb
@@ -0,0 +1,7 @@
+module Ruboty
+  module Slack
+    module Button
+      VERSION = "0.1.0"
+    end
+  end
+end
