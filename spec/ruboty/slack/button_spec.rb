0282dc8d2962edd9e65f0144a35746f8ce834bb2
hkdnet <hkdnet@users.noreply.github.com>
2017-02-08 05:14:24 +0900

    Remove unnecessary test

--- spec/ruboty/slack/button_spec.rb
+++ spec/ruboty/slack/button_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Ruboty::Slack::Button do
   it "has a version number" do
     expect(Ruboty::Slack::Button::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
