54b6c0b42473cf99cf9db66c7abb51667de52649
hkdnet <hkdnet@users.noreply.github.com>
2017-02-12 18:23:08 +0900

    Creator should accept button_message
    It should not have the responsibility of create ButtonMessage

--- lib/ruboty/slack_button/creator.rb
+++ lib/ruboty/slack_button/creator.rb
@@ -1,8 +1,10 @@
 require 'ruboty/slack_button/client'
+require 'ruboty/slack_button/message'
 
 module Ruboty
   module SlackButton
     class Creator
+      attr_reader :message, :username, :icon_emoji, :channel, :webhook_url
       def initialize(message, username: nil, icon_emoji: nil, channel: nil, webhook_url: nil)
         @message = message
         @username = username
@@ -11,8 +13,20 @@ module Ruboty
         @webhook_url = webhook_url
       end
 
-      def create
-        client.post(payload, headers)
+      def create_button(button_message)
+        client.post(payload(button_message), headers)
+      end
+
+      def payload(button_message)
+        payload_object(button_message).to_json
+      end
+
+      def payload_object(button_message)
+        button_message.as_json.merge({
+          channel: channel,
+          icon_emoji: icon_emoji,
+          username: username,
+        }).compact
       end
 
       def client
@@ -31,22 +45,6 @@ module Ruboty
 
       def webhook_url
         @webhook_url ||= ENV['SLACK_BUTTON_WEBHOOK_URL']
-      end
-
-      def payload
-        payload_object.to_json
-      end
-
-      def payload_object
-        button_message.merge({
-          channel: channel,
-          icon_emoji: icon_emoji,
-          username: username,
-        })
-      end
-
-      def button_message
-        @button_message ||= ButtonMessage.new
       end
     end
   end
