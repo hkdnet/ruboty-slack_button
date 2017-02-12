2d8ab9eced161a2e841efa97c009c76db4bb527b
hkdnet <hkdnet@users.noreply.github.com>
2017-02-12 20:23:55 +0900

    Expect message.as_json returns the same object

--- spec/ruboty/slack_button/models/message_spec.rb
+++ spec/ruboty/slack_button/models/message_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe Ruboty::SlackButton::Models::Message do
   describe '#as_json' do
-    let(:message) { Ruboty::SlackButton::Models::Message.new }
+    let(:message) { Ruboty::SlackButton::Models::Message.new(text: text, attachments: attachments) }
+    let(:text) { "text" }
+    let(:attachments) { [] }
     it { expect(message.as_json).to include :text }
     it { expect(message.as_json).to include :attachments }
+    it { expect(message.as_json[:text]).to be text }
+    it { expect(message.as_json[:attachments]).to be attachments }
   end
 end
